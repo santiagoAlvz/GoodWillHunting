228d2bfc10765fd1518f87b6767f8042b5a6f44e
Santiago Álvarez <A01640172@itesm.mx>
2023-03-15 10:43:04 -0600

    added arguments for changing output file and outputing the arrangements table

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,56 @@
 using std::cout;
 using std::endl;
 
+struct PARAMETERS{
+    int n;
+    bool verbose = false;
+    string output = "output";
+};
+
+void readArguments(int argc, char* argv[], PARAMETERS &rP){
+    bool specifiedN = false;
+
+    for(int i = 1; i < argc; i++){
+        if(argv[i][0] == '-'){
+            switch(argv[i][1]){
+                case 'o':
+                    i++;
+                    rP.output = argv[i];
+                    break;
+                case 'v':
+                    rP.verbose = true;
+                    break;
+                default:
+                    throw("Unknown argument -" + argv[i][1]);
+            }
+        } else {
+            specifiedN = true;
+            rP.n = atoi(argv[i]);
+        }
+    }
+
+    if(!specifiedN) throw("Missing N value");
+}
+
 int main(int argc, char* argv[]) {
 
-    if(argc < 2){
-        cout<<"N value wasn't provided"<<endl;
+    PARAMETERS runtimeParameters;
+
+    try{
+        readArguments(argc, argv, runtimeParameters);
+    } catch(char const* message){
+        cout<<message<<endl;
         return 1;
     }
 
-    unsigned n = atoi(argv[1]);
-
-    ArrangementsTable arrangements(n);
+    ArrangementsTable arrangements(runtimeParameters.n);
 	FinalTreeMaker tree(&arrangements);
-	tree.createArrangements(n);
+	tree.createArrangements(runtimeParameters.n);
+
+	if(runtimeParameters.verbose) cout<<arrangements<<endl;
+
 	arrangements.printStats();
-	arrangements.printToFile("output",n);
+	arrangements.printToFile(runtimeParameters.output, runtimeParameters.n);
 
 	return 0;
 }
