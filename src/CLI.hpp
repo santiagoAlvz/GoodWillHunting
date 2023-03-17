b561472492b7d523ea98e737a35614b3cf1d5032
Santiago Álvarez <A01640172@itesm.mx>
2023-03-16 19:51:27 -0600

    improved argument detection

---
+++ src/CLI.hpp
@@ -0,0 +1,72 @@
+#ifndef CLI_H
+#define CLI_H
+
+#define VALUE_REQUIRED_STRING 1
+#define NO_VALUE_BOOL 0
+
+struct arguments{
+	int n;
+	string output = "output";
+	bool verbose = false;
+	bool help = false;
+};
+
+arguments runtimeArgs;
+
+struct option{
+	char c;
+	string name;
+	int type;
+	void* argument;
+};
+
+static struct option const options[] = {
+	{'o',"output",VALUE_REQUIRED_STRING, &runtimeArgs.output},
+	{'v',"verbose",NO_VALUE_BOOL, &runtimeArgs.verbose}
+};
+
+void readArguments(int argc, char* argv[]){
+	string element;
+	bool found;
+	bool isNPresent = false;
+
+	for(int i = 1; i < argc; i++){
+		element = argv[i];
+
+
+		if(element[0] == '-'){
+			found = false;
+			for(unsigned long j = 0; !found && j < sizeof(options) / sizeof(option); j++){
+				if((element.size() > 1 && element[1] == options[j].c) ||
+				(element.substr(2, element.size() - 2) == options[j].name)){
+					switch(options[j].type){
+						case VALUE_REQUIRED_STRING:
+							i++;
+							if(i == argc || argv[i][0] == '-') throw ("Missing value for argument "+element);
+
+							*(string*)(options[j].argument) = argv[i];
+							break;
+						case NO_VALUE_BOOL:
+							*(bool*)(options[j].argument) = true;
+							break;
+					}
+					found = true;
+				}
+			}
+
+			if(!found){
+				throw ("Unknown argument "+element);
+			}
+
+		} else {
+			if(std::all_of(element.begin(), element.end(), ::isdigit)){
+				runtimeArgs.n = stoi(element);
+				isNPresent = true;
+			} else throw ("Unexpected value '" + element + "'");
+		}
+	}
+
+	if (!isNPresent) throw (string("Missing N value"));
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,27 @@
 #include <iostream>
 #include "FinalTreeMaker.hpp"
+#include "CLI.hpp"
 
 using std::cout;
 using std::endl;
 
-struct PARAMETERS{
-    int n;
-    bool verbose = false;
-    string output = "output";
-};
-
-void readArguments(int argc, char* argv[], PARAMETERS &rP){
-    bool specifiedN = false;
-
-    for(int i = 1; i < argc; i++){
-        if(argv[i][0] == '-'){
-            switch(argv[i][1]){
-                case 'o':
-                    i++;
-                    rP.output = argv[i];
-                    break;
-                case 'v':
-                    rP.verbose = true;
-                    break;
-                case 'h':
-                    throw("");
-                default:
-                    throw("Unknown argument -" + argv[i][1]);
-            }
-        } else {
-            specifiedN = true;
-            rP.n = atoi(argv[i]);
-        }
-    }
-
-    if(!specifiedN) throw("Missing N value");
-}
-
-void printHelp(){
-    cout<<"usage: goodwillhunting [options] N"<<endl;
-
-    cout<<"options:"<<endl;
-    cout<<"\t-o [filename]\tProvides a custom filename for the output, if not specified, 'output' is used"<<endl;
-    cout<<"\t-v\t(from verbose) prints the arrangement matrix"<<endl;
-}
-
 int main(int argc, char* argv[]) {
 
-    PARAMETERS runtimeParameters;
-
-    try{
-        readArguments(argc, argv, runtimeParameters);
-    } catch(char const* message){
+    try {
+        readArguments(argc, argv);
+    } catch (string message){
         cout<<message<<endl;
-        printHelp();
         return 1;
     }
 
-    ArrangementsTable arrangements(runtimeParameters.n);
+    ArrangementsTable arrangements(runtimeArgs.n);
 	FinalTreeMaker tree(&arrangements);
-	tree.createArrangements(runtimeParameters.n);
+	tree.createArrangements(runtimeArgs.n);
 
-	if(runtimeParameters.verbose) cout<<arrangements<<endl;
+	if(runtimeArgs.verbose) cout<<arrangements<<endl;
 
 	arrangements.printStats();
-	arrangements.printToFile(runtimeParameters.output, runtimeParameters.n);
+	arrangements.printToFile(runtimeArgs.output, runtimeArgs.n);
 
 	return 0;
 }
