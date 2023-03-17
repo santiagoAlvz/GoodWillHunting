a8b637af4d616fbc6c17fb16ba10da418e1a98ce
Santiago Álvarez <A01640172@itesm.mx>
2023-03-17 17:16:32 -0600

    added support for short option grouping

--- src/CLI.hpp
+++ src/CLI.hpp
@@ -1,8 +1,10 @@
 #ifndef CLI_H
 #define CLI_H
 
-#define VALUE_REQUIRED_STRING 1
-#define NO_VALUE_BOOL 0
+#define VALUE_REQUIRED_STRING 0
+#define NO_VALUE_BOOL 1
+
+#include <queue>
 
 struct arguments{
 	int n;
@@ -25,7 +27,7 @@ struct option{
 static struct option const options[] = {
 	{'o',"output",VALUE_REQUIRED_STRING, &runtimeArgs.output,"provide a custom filename for the output, if unspecified, 'output' will be used", " FILENAME"},
 	{'v',"verbose",NO_VALUE_BOOL, &runtimeArgs.verbose, "display the complete arrangements table",""},
-	{'h',"help",NO_VALUE_BOOL, &runtimeArgs.help, "displays program help",""}
+	{'h',"help",NO_VALUE_BOOL, &runtimeArgs.help, "display program help",""}
 };
 
 void printHelp(){
@@ -38,13 +40,23 @@ void printHelp(){
 }
 
 void readArguments(int argc, char* argv[]){
+	std::queue<string> clarguments;
 	string element;
 	bool found;
 	bool isNPresent = false;
 
 	for(int i = 1; i < argc; i++){
-		element = argv[i];
+		if(argv[i][0] == '-' && argv[i][1] != '-'){
+			element = argv[i];
+			for(unsigned long j = 1; j < element.size(); j++){
+				clarguments.push(string() + '-' + element[j]);
+			}
+		} else clarguments.push(string(argv[i]));
+	}
 
+	while (!clarguments.empty()){
+		element = clarguments.front();
+		clarguments.pop();
 
 		if(element[0] == '-'){
 			found = false;
@@ -53,10 +65,12 @@ void readArguments(int argc, char* argv[]){
 				(element.substr(2, element.size() - 2) == options[j].name)){
 					switch(options[j].type){
 						case VALUE_REQUIRED_STRING:
-							i++;
-							if(i == argc || argv[i][0] == '-') throw ("Missing value for argument "+element);
 
-							*(string*)(options[j].argument) = argv[i];
+							//If there aren't more arguments or if next argument is an option
+							if(clarguments.empty() || clarguments.front()[0] == '-') throw ("Missing value for argument "+element);
+
+							*(string*)(options[j].argument) = clarguments.front();
+							clarguments.pop();
 							break;
 						case NO_VALUE_BOOL:
 							*(bool*)(options[j].argument) = true;
@@ -72,7 +86,7 @@ void readArguments(int argc, char* argv[]){
 
 		} else {
 			if(std::all_of(element.begin(), element.end(), ::isdigit)){
-				runtimeArgs.n = stoi(element);
+				runtimeArgs.n = std::stoi(element);
 				isNPresent = true;
 			} else throw ("Unexpected value '" + element + "'");
 		}
