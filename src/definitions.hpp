3ffacbe72620ec6278e4db743f5b8ad77dd86918
Santiago Álvarez <A01640172@itesm.mx>
2023-03-25 17:41:23 -0600

    added comments to definitions.hpp

--- src/definitions.hpp
+++ src/definitions.hpp
@@ -1,12 +1,24 @@
+/* Type definitions to be used in the rest of the program
+ */
+
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
 #include <utility>
 #include <vector>
 
+//an edge. It contains the two nodes a given edge connects
 typedef std::pair<unsigned, unsigned> edge;
+
+//a list of edges
 typedef std::vector<edge> edgeList;
+
+//a seed is the list of all non-leaf edge counts for a given tree
 typedef std::vector<unsigned> seed;
+
+//a leaf node cluster is a group of leaf nodes that are connected to the same
+// branch node. This pair contains how many nodes are between the leaf nodes and
+// the "core" node, and the amount of nodes of the cluster
 typedef std::pair<unsigned, unsigned> leafNodeCluster;
 
 #endif
